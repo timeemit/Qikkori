083dbfb1825efc643cf80be3aca4a659cba79ce5
TimeEmit <TimeEmit@sdf.lonestar.org>
2012-02-17 18:05:52 -0500

    Migrated DB. Validating User model. Writing RSpec unit tests for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+  validates :username, :length => {:in => 4..20}
+  validates :email, :confirmation => true
+  validates :email_confirmation, :presence => true
+  validates :password, :confirmation => true
+  validates :password_confirmation, :presence => true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.new(
+    :username => 'girl', 
+    :email => 'liam@infobank.com',
+    :email_confirmation => 'liam@infobank.com',
+    :password => 'LiamisGre4t',
+    :password_confirmation => 'LiamisGre4t'  
+  )
+
+  describe "validations" do
+    it "should make sure the username isn't too short or long" do
+      user.stub(:username) {'hey'}
+      user.save.should_not be_true
+      user.stub(:username) {'012345678901234567890'}
+      user.save.should_not be_true
+    end
+    
+    it "should confirm password upon creation" do
+      user.stub(:password_confirmation) {'LiamIsTerrible'}
+      user.save.should_not be_true
+    end
+
+    it "should validate that password is not nil" do
+      user.stub(:password_confirmation) {nil}
+      user.save.should_not be_true
+    end
+
+    it "should confirm email upon creation" do
+      user.stub(:email_confirmation) {'LiamIsTerrible'}
+      user.save.should_not be_true
+    end
+
+    it "should validate that email is not nil" do
+      user.stub(:email_confirmation) {nil}
+      user.save.should_not be_true
+    end
+
+    it "should let the gal signup" do
+      user.save.should be_true
+    end
+  end
+
 end
