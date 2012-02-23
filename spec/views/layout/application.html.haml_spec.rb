3ef79020d9c149ab8c7260303e0ebf48f0d58f45
TimeEmit <TimeEmit@sdf.lonestar.org>
2012-02-23 12:00:42 -0500

    Figured out that the included libraries cannot be tested to exist in the DOM because they are all grouped into the application.js file from the asset pipeline.

--- spec/views/layout/application.html.haml_spec.rb
+++ spec/views/layout/application.html.haml_spec.rb
@@ -1,29 +1,35 @@
 require 'spec_helper'
 
-describe "layout" do
+describe "the layout" do
 
-  it 'has the title Qikkori' do
-    assign(:user, stub_model(User,
-      :username => "girl",
-      :email => "liam@infobank.com",
-      :password => "LiamisGre4t"
-    ).as_new_record)
-    render :template => 'users/new', :layout => 'layouts/application'
-    assert_select 'title', /Qikkori/
-  end
+  context 'when looking at users' do
+    before(:each) do
+      assign(:user, stub_model(User,
+        :username => "girl",
+        :email => "liam@infobank.com",
+        :password => "LiamisGre4t"
+      ).as_new_record)
+    end
+    
+    it 'has the title Qikkori' do
+      render :template => 'users/new', :layout => 'layouts/application'
+      assert_select 'title', /Qikkori/
+    end
   
-  it "includes the jQuery, Underscore, and Backbone javascript libraries" do
-    assign(:user, stub_model(User,
-      :username => "girl",
-      :email => "liam@infobank.com",
-      :password => "LiamisGre4t"
-    ).as_new_record)
-    render :template => 'users/new', :layout => 'layouts/application'
-    assert_select "[src=?]", "/assets/jquery.js?body=1"
-    assert_select "[src*=?]", 'jquery_ujs.js'
-    assert_select "[src*=?]", 'jquery_ui.js'
-    assert_select "[src*=?]", 'underscore.js'
-    assert_select "[src*=?]", 'backbone.js'
+    it "includes the jQuery, Underscore, and Backbone javascript libraries" do
+      render :template => 'users/new', :layout => 'layouts/application'
+      scripts = css_select "script"
+      assert !scripts.empty?
+      assert_select "script"
+      assert_select "[type=?]", "text/javascript"
+      assert_select "[src*=?]", 'application.js'
+      # The following asserts WILL NOT work in the test environment
+      # because the asset pipeline groups all of these files into the Application.js file.
+      # assert_select "[src=?]", "jquery.js"        
+      # assert_select "[src*=?]", 'jquery_ujs.js'
+      # assert_select "[src*=?]", 'jquery_ui.js'
+      # assert_select "[src*=?]", 'underscore.js'
+      # assert_select "[src*=?]", 'backbone.js'
+    end
   end
-
 end
